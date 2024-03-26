a8d74b3c0d7afb72dae6af1e341df6ab5b6c33d6
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-26 19:05:46 -0300

    Add state verifier code

---
+++ src/lib/state_verifier/state_verifier.ml
@@ -0,0 +1,17 @@
+(* let read_whole_file filename =
+  (* open_in_bin works correctly on Unix and Windows *)
+  let ch = open_in_bin filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch ; s
+let proof = read_whole_file "../../../../proof.txt"
+let decoded_proof = Mina_block.Precomputed.Proof.of_bin_string proof *)
+
+let serialized_block =
+  External_transition_sample_precomputed_block.sample_block_json
+
+(* let block = Yojson.Safe.from_string serialized_block *)
+
+(* 
+Verifier.verify_blockchain_snarks verifier, to_verify
+*)
+
