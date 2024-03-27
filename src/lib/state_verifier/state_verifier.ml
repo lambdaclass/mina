c3a7dffe3e28f3e781e2d18e6e2854732b4522b5
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 20:20:59 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -22,8 +22,9 @@ let proof = block.protocol_state_proof
 
 let blockchain = Blockchain_snark.Blockchain.create ~state ~proof
 
-let () = Parallel.init_master ();
 let logger = Logger.create ()
+
+let () = Parallel.init_master ()
 
 (* let conf_dir = Cli_lib.Flag.conf_dir *)
 
