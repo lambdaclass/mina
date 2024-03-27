8c2594297b6d636e5429ddb161a74fe6021a9e80
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 16:55:43 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -15,22 +15,6 @@ let result = Mina_block.Precomputed.of_yojson json_block
 
 let block = Result.get_ok result
 
-(* let convert_to_stable_v1 (block_abs : Mina_block.Precomputed.t) :
-     Mina_block.Precomputed.Stable.V1.t =
-   let scheduled_time = block_abs.scheduled_time in
-   let protocol_state = block_abs.protocol_state in
-   let protocol_state_proof = block_abs.protocol_state_proof in
-   let staged_ledger_diff = block_abs.staged_ledger_diff in
-   let delta_transition_chain_proof = block_abs.delta_transition_chain_proof in
-   { Mina_block.Precomputed.Stable.V1.scheduled_time
-   ; protocol_state
-   ; protocol_state_proof
-   ; staged_ledger_diff
-   ; delta_transition_chain_proof
-   } *)
-
-(* let v1_block = convert_to_stable_v1 block *)
-
 let state : Mina_state.Protocol_state.Value.Stable.Latest.t =
   block.protocol_state
 
@@ -38,11 +22,7 @@ let proof = block.protocol_state_proof
 
 let blockchain = Blockchain_snark.Blockchain.create ~state ~proof
 
-(* let proof = Mina_block.Precomputed.protocol_state_proof block *)
-
-(* let header = Mina_block.Precomputed.header block *)
-
-let () = print_endline block
+let () = print_endline "State verified successfully"
 
 (*
    Verifier.verify_blockchain_snarks verifier, to_verify
