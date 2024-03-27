283dd4617e0181a01a1442943a3cdb57f8a22a0c
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 17:42:26 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -7,7 +7,7 @@ let json_block = Yojson.Safe.from_string serialized_block
 
 let result = Mina_block.Precomputed.of_yojson json_block
 
-let block = Result.get_ok result
+let block = Core_kernel.get_ok result
 
 let state : Mina_state.Protocol_state.Value.Stable.Latest.t =
   block.protocol_state
