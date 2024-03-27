6186aeff13b8fb8a9313b4fbbe35943736c50c69
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 16:40:56 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -31,7 +31,7 @@ let block = Result.get_ok result
 
 (* let v1_block = convert_to_stable_v1 block *)
 
-let state = block.protocol_state
+let state = Mina_state.Protocol_state.Value.t block.protocol_state
 
 let proof = block.protocol_state_proof
 
