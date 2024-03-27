1d774ce0f81df697fd92878b7225eef7efebf722
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 11:45:47 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -12,7 +12,7 @@ let serialized_block =
 let block = Yojson.Safe.from_string serialized_block
 
 let () =
-  match block with Ok block_inner -> print_endline block_inner | _ -> ()
+  match block with Ok block_inner -> print_endline block_inner | Error _ -> ()
 
 (* let () = print_endline block *)
 
