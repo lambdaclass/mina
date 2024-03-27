5c4bec1d8543120ad773bcf549c2e723f96e9c98
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 17:22:23 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -17,10 +17,11 @@ let _blockchain = Blockchain_snark.Blockchain.create ~state ~proof
 let logger = Logger.create ()
 
 let conf_dir = Cli_lib.Flag.conf_dir
-(* let verifier =
-   Verifier.create ~logger ~proof_level:Genesis_constants.Proof_level.compiled
-     ~constraint_constants:Genesis_constants.Constraint_constants.compiled
-     ~pids:(Pid.Table.create ()) ~conf_dir:(Some conf_dir) *)
+
+let verifier =
+  Verifier.create ~logger ~proof_level:Genesis_constants.Proof_level.compiled
+    ~constraint_constants:Genesis_constants.Constraint_constants.compiled
+    ~pids:(Pid.Table.create ()) ~conf_dir:(Some conf_dir)
 
 let () = print_endline "State verified successfully"
 
