1b2e6b5315da214e0760922eb603f6e1924f6cd4
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 18:17:03 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -29,7 +29,7 @@ let conf_dir = Cli_lib.Flag.conf_dir
 let verifier =
   Verifier.create ~logger ~proof_level:Genesis_constants.Proof_level.compiled
     ~constraint_constants:Genesis_constants.Constraint_constants.compiled
-    ~pids:Child_processes.Termination.create_pid_table ()
+    ~pids:(Child_processes.Termination.create_pid_table ())
     ~conf_dir:(Some conf_dir)
 
 let () = print_endline "State verified successfully"
