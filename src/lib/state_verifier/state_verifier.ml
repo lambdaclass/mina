615cd795821d908a84092b7c5f803f4e9974de3b
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 16:56:32 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -20,7 +20,7 @@ let state : Mina_state.Protocol_state.Value.Stable.Latest.t =
 
 let proof = block.protocol_state_proof
 
-let blockchain = Blockchain_snark.Blockchain.create ~state ~proof
+let _blockchain = Blockchain_snark.Blockchain.create ~state ~proof
 
 let () = print_endline "State verified successfully"
 
