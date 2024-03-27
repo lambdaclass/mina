f3b914547d4569bf5f9701af42f19d8884ccc08a
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 18:42:02 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -26,21 +26,12 @@ let logger = Logger.create ()
 
 let conf_dir = Cli_lib.Flag.conf_dir
 
-let%bind verifier =
-  Verifier.create~logger ~proof_level:Genesis_constants.Proof_level.compiled
-    ~constraint_constants:Genesis_constants.Constraint_constants.compiled
-    ~pids:(Child_processes.Termination.create_pid_table ())
-    ~conf_dir:None in
-  let%bind result = (Verifier.verify_blockchain_snarks verifier, [ blockchain ])
-in
-match result with
-| Ok true -> 
-  printf "Proofs verified successfully" ;
-  exit 0
-| Ok false ->
-  printf "Proofs failed to verify" ;
-  exit 1
-| Error err ->
-  printf "Failed while verifying proofs:\n%s"
-    (Error.to_string_hum err) ;
-  exit 2;
+let verifier =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      Verifier.create ~logger
+        ~proof_level:Genesis_constants.Proof_level.compiled
+        ~constraint_constants:Genesis_constants.Constraint_constants.compiled
+        ~pids:(Child_processes.Termination.create_pid_table ())
+        ~conf_dir:None )
+
+let result = (Verifier.verify_blockchain_snarks verifier, [ blockchain ])
