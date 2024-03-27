e94ff2ff9d5c17b8aceb8953c47bcbc5db4a9ee5
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-27 12:16:47 -0300

    Dummy

--- src/lib/state_verifier/state_verifier.ml
+++ src/lib/state_verifier/state_verifier.ml
@@ -11,8 +11,16 @@ let serialized_block =
 
 let json_block = Yojson.Safe.from_string serialized_block
 
+(* let block = *)
+(* match Mina_block.Precomputed.of_yojson json_block with
+   | Ok block ->
+       block
+   | Error err ->
+       failwithf "Could not read block: %s" err () *)
 let block =
-  match Mina_block.Precomputed.of_yojson json_block with
+  match
+    Yojson.Safe.from_string json_block |> Mina_block.Precomputed.of_yojson
+  with
   | Ok block ->
       block
   | Error err ->
